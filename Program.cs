9b8f89495f95dd41839fcb61bb45ce7c4f887135
João Paulo <joaopdejesus79@gmail.com>
2021-09-25 12:31:06 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, p, i;
+            
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n=== Índice de Massa Corporal ===\n");
+            Console.ResetColor();
+
+            Console.WriteLine("Indique a altura em metros e o\npeso em kg, para calcular o IMC.\n");
+
+            Console.Write("Altura...: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Peso.....: ");
+            p = Convert.ToDouble(Console.ReadLine());
+
+            i = p / Math.Pow(a, 2);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n=== IMC ===\n");
+            Console.ResetColor();
+
+            Console.WriteLine($"IMC......: {i:N1} kg/m²\n");
+
+        }
+    }
+}
